28758679d93048ceee0a470d2fc16382047a2c80
Francesco Santacroce <santa@Santa-2.local>
2019-07-16 12:38:45 +0200

    Riprovo a caricare

---
+++ index.js
@@ -0,0 +1,200 @@
+const express = require('express');
+const app = express();
+const Datastore = require('nedb');
+require('dotenv').config();
+var loggedIn = false;
+const date = new Date();
+
+const Coinqui = [
+    {
+        "name": "Francesco",
+        "surname": "Santacroce",
+        "nickName": "Santa",
+        "password": "puliscoCesso"
+    },
+    {
+        "name": "Maurizio",
+        "surname": "Pinna",
+        "nickName": "Mauri",
+        "password": "puliscoCucina"
+    },
+    {
+        "name": "Lorenzo",
+        "surname": "Meli",
+        "nickName": "Lori",
+        "password": "puliscoPavimento"
+    }
+];
+
+
+app.listen(3000, () => console.log('Listening at port: 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb'}));
+
+const databaseLogin = new Datastore({ filename: 'loginDatabase.db', autoload: true });
+databaseLogin.loadDatabase();
+
+const databaseTurniCucina = new Datastore({ filename: 'turniCucinaDatabase.db', autoload: true });
+databaseTurniCucina.loadDatabase();
+
+const databaseTurniCesso = new Datastore({ filename: 'turniCessoDatabase.db', autoload: true });
+databaseTurniCesso.loadDatabase();
+
+const databaseTurniPiattiTrio = new Datastore({ filename: 'turniPiattiTrioDatabase.db', autoload: true });
+databaseTurniPiattiTrio.loadDatabase();
+
+const databaseTurniPiattiSantaMauri = new Datastore({ filename: 'turniPiattiSantaMauriDatabase.db', autoload: true });
+databaseTurniPiattiSantaMauri.loadDatabase();
+
+const databaseTurniPiattiSantaLori = new Datastore({ filename: 'turniPiattiSantaLoriDatabase.db', autoload: true });
+databaseTurniPiattiSantaLori.loadDatabase();
+
+const databaseTurniPiattiMauriLori = new Datastore({ filename: 'turniPiattiLoriMauriDatabase.db', autoload: true });
+databaseTurniPiattiMauriLori.loadDatabase();
+
+app.get('/getTurni0', (request, response) => {
+    databaseTurniCucina.find({}).sort({timeStamp: 1}).exec((err, data) => {
+        if (err) {
+            response.end()
+            return
+        } else {
+        response.json(data)}
+    });
+})
+
+app.post('/postTurni0', (request, response) => {
+    var nickname = request.body.nickname
+    var timestamp = Date.now();
+    request.body.timeStamp = timestamp;
+    var date = new Date(parseInt(timestamp));
+    request.body.date = date.toLocaleString();
+    databaseTurniCucina.insert(request.body);
+    response.end();
+})
+
+app.post('/postTurni1', (request, response) => {
+    var nickname = request.body.nickname
+    var timestamp = Date.now();
+    request.body.timeStamp = timestamp;
+    databaseTurniCesso.insert(request.body);
+    response.end();
+})
+
+app.get('/getTurni1', (request, response) => {
+    databaseTurniCesso.find({},(err, data) => {
+        if (err) {
+            response.end()
+            return
+        } else {
+        response.json(data)}
+    });
+})
+
+app.post('/postTurni2', (request, response) => {
+    var nickname = request.body.nickname
+    var timestamp = Date.now();
+    request.body.timeStamp = timestamp;
+    databaseTurniPiattiTrio.insert(request.body);
+    response.end();
+})
+
+app.get('/getTurni2', (request, response) => {
+    databaseTurniPiattiTrio.find({},(err, data) => {
+        if (err) {
+            response.end()
+            return
+        } else {
+        response.json(data)}
+    });
+})
+
+app.post('/postTurni3', (request, response) => {
+    var nickname = request.body.nickname
+    var timestamp = Date.now();
+    request.body.timeStamp = timestamp;
+    databaseTurniPiattiSantaMauri.insert(request.body);
+    response.end();
+})
+
+app.get('/getTurni3', (request, response) => {
+    databaseTurniPiattiSantaMauri.find({},(err, data) => {
+        if (err) {
+            response.end()
+            return
+        } else {
+        response.json(data)}
+    });
+})
+
+app.post('/postTurni4', (request, response) => {
+    var nickname = request.body.nickname
+    var timestamp = Date.now();
+    request.body.timeStamp = timestamp;
+    databaseTurniPiattiSantaLori.insert(request.body);
+    response.end();
+})
+
+app.get('/getTurni4', (request, response) => {
+    databaseTurniPiattiSantaLori.find({},(err, data) => {
+        if (err) {
+            response.end()
+            return
+        } else {
+        response.json(data)}
+    });
+})
+
+app.post('/postTurni5', (request, response) => {
+    var nickname = request.body.nickname
+    var timestamp = Date.now();
+    request.body.timeStamp = timestamp;
+    databaseTurniPiattiMauriLori.insert(request.body);
+    response.end();
+})
+
+app.get('/getTurni5', (request, response) => {
+    databaseTurniPiattiMauriLori.find({},(err, data) => {
+        if (err) {
+            response.end()
+            return
+        } else {
+        response.json(data)}
+    });
+})
+
+
+
+app.post('/loginRequest', (request, response) => {
+    var timestamp = Date.now();
+    const username = request.body.username;
+    const password = request.body.password;
+    request.body.timeStamp = timestamp;
+
+    if (Coinqui) {
+        for (var i in Coinqui){
+            if (Coinqui[i].nickName.toLowerCase() == username.toLowerCase()){
+                if (Coinqui[i].password == password){   
+                    loggedIn = true;      
+                    loggedUser = Coinqui[i].name + ' ' +Coinqui[i].surname;
+                    loggedNickname = Coinqui[i].nickName;
+                    response.json({status: "success", error: "null", loggeduser: loggedUser, loggednickname: loggedNickname });
+                    databaseLogin.insert(request.body)
+                    response.end();
+                    return
+                } else {
+                    response.json({status: "failed", error: "password", loggeduser: null, loggednickname: null });
+                    databaseLogin.insert(request.body)
+                    response.end();
+                    return
+                }
+            };
+        };
+        if (!loggedIn){
+            response.json({status: "failed", error: "credentials", loggeduser: null, loggednickname: null });
+                    databaseLogin.insert(request.body)
+                    response.end();
+                    return
+        };
+    }
+
+})
---
+++ public/script.js
@@ -0,0 +1,139 @@
+var tabButton=document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanel= document.querySelectorAll(".tabContainer .tabPanel");
+var tableLordi=document.querySelector(".Maintable")
+var tableLogin=document.querySelector(".loginForm")
+var loggedUser;
+var loggedNickname;
+var date = new Date();
+var colorCode = '#f44336';
+var panelIndex;
+var chosenTab = 0;
+var Coinqui;
+var Turni;
+const headr = "<tr><th>Lordazzo</th><th>Data</th></tr>";
+var turno= new Array;
+var tableLordazzi = document.getElementById("tableLordazzi");
+
+
+
+
+function getInfo() {
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    const loginData = { username, password};
+    const options = {
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginData)
+    }
+    loginRequest();
+    
+    async function loginRequest(){
+        const response = await fetch('/loginRequest', options);
+        const data = await response.json();
+        switch (data.status) {
+            case 'success': 
+                        loggedUser = data.loggeduser;
+                        loggedNickname = data.loggednickname;
+                        updateUser()
+                        break;
+            case 'failed':
+                if (data.error == 'credentials') {
+                            alert('Wrong credentials')
+                            return
+                } else if (data.error == 'password'){
+                            alert('Wrong password')
+                            return
+                }
+        }
+    };
+    
+};
+
+function updateUser() {
+    tableLogin.style.display="none";
+    tableLordi.style.display="block";
+    showPanel(0);
+    document.getElementById("userLog").textContent = 'Logged user: '+loggedUser;
+};
+
+function showPanel(panelIndex) {
+    chosenTab = panelIndex;
+
+    tabButton.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color = "";
+    });
+
+    tabButton[panelIndex].style.backgroundColor=colorCode;
+    tabButton[panelIndex].style.color="white";
+    
+    tabPanel[0].style.display="block";
+    tabPanel[0].style.backgroundColor=colorCode;
+    tableLordazzi.textContent = "";
+    var row;
+    var cell
+    row = document.createElement('tr');
+    cell = document.createElement('th');
+    cell.appendChild(document.createTextNode('Lordazzo'));
+    row.appendChild(cell);
+    cell = document.createElement('th');
+    cell.appendChild(document.createTextNode('Data'));
+    row.appendChild(cell);
+    
+    tableLordazzi.appendChild(row);
+    
+    getTurni();
+    
+    async function getTurni(){
+        const response = await fetch('/getTurni'+chosenTab);
+        const data = await response.json();
+
+        Turni = data;
+        
+        const numTurni = Object.keys(Turni).length;
+        const TurniMin = (numTurni > 5) ? numTurni-5 : 0;
+        
+        
+        for (var j = TurniMin; j<numTurni; j++){
+            row = document.createElement('tr');
+            cell = document.createElement('td');
+            cell.appendChild(document.createTextNode(Turni[j].loggedNickname));
+            row.appendChild(cell);
+            cell = document.createElement('td');
+            var date = new Date(parseInt(Turni[j].timeStamp));
+            date = date.toLocaleString();
+            cell.appendChild(document.createTextNode(date));
+            row.appendChild(cell);
+            tableLordazzi.appendChild(row);
+        }
+        
+    }
+};
+
+function lavoIo (){
+    var j = 0;
+
+    const turno = {loggedNickname}
+    const options = {
+        method: 'POST',
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(turno)
+    }
+    postTurni();
+
+    async function postTurni() {
+        const response = await fetch('/postTurni'+chosenTab, options);
+    }
+
+    
+
+    alert(loggedUser+' lava alle '+ date.getHours()+':'+date.getMinutes()+' il '+date.getDate()+'-'+(date.getMonth()+1)+'-'+date.getFullYear());
+    showPanel(chosenTab);
+}
